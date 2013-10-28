8af171600ab0aeb971a352b90810b8ee084b1ada
Mark Pinese <m.pinese@garvan.org.au>
2013-10-28 15:37:59 +1100

    Auto-commit

---
+++ R/messina-package.R
@@ -0,0 +1,28 @@
+#' TODO
+#'
+#' \tabular{11}{
+#' Package: \tab messina\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.99.0\cr
+#' Date: \tab TODO\cr
+#' License: \tab EPL (>= 1.0)\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' Using the Fermat primality test, pseudoprime checks for primes
+#' probabilistically; the test is fooled every time by Carmichael
+#' numbers.
+#'
+#' \code{\link{is.pseudoprime}} checks a number \code{n} for
+#' pseudoprimality, applying Fermat?s test \code{times} times.
+#'
+#' @name messina-package
+#' @aliases messina
+#' @docType package
+#' @title TODO
+#' @author Mark Pinese \email{m.pinese@@garvan.org.au}
+#' @references
+#' \url{TODO}
+#' @keywords package
+#' @seealso \code{\link{TODO}}
+NULL
